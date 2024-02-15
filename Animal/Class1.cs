58a7172fc9890287ba8d7c8d2fd3347fad39b9f7
vladspiridonov <vlad777.96@mail.ru>
2024-02-15 23:53:30 +0300

    Добавьте файлы проекта.

---
+++ Animal/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.Write($"Имя: {Name}  Говорит:  ");
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        string name;
+        string sound;        
+        public override string Name 
+        { 
+            get => name; 
+            set => name=value; 
+        }
+        public Cat(string name, string sound)
+            :base(name)        
+        {           
+            this.sound = sound;
+        }
+
+        public override void Say()               
+        {
+            Console.WriteLine(sound);
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        string sound;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string sound)
+            : base(name)
+        {
+            this.sound = sound;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+}
---
+++ Animal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("bobik", "bark");
+            Cat cat = new Cat("Murzik", "meow");
+            dog.ShowInfo();
+            cat.ShowInfo();
+            dog.Say();
+            cat.Say();
+            Console.ReadKey();
+        }
+    }
+}
